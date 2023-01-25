b6969f7a73e2dc710a80c6d59e3a3134aee6e5ff
Denis Telnov <denis.telnov@gmail.com>
2023-01-26 00:22:48 +0300

    Add `ndims` for NZ iterators

--- src/DensedSparseVectors.jl
+++ src/DensedSparseVectors.jl
@@ -262,6 +262,8 @@ function Base.collect(::Type{ElType}, V::AbstractDensedSparseVector) where ElTyp
 end
 Base.collect(V::AbstractDensedSparseVector) = collect(eltype(V), V)
 
+@inline nnzchunks(V::Vector) = 1
+@inline nnzchunks(V::SparseVector) = length(iteratenzchunks(V))
 @inline nnzchunks(V::AbstractDensedSparseVector) = length(getfield(V, :nzchunks))
 @inline nnzchunks(V::OffsetArray{<:Any,<:Any,<:T}) where {T<:AbstractDensedSparseVector} = nnzchunks(parent(V))
 @inline length_of_that_nzchunk(V::AbstractVectorDensedSparseVector, chunk) = length(chunk)
@@ -774,6 +776,7 @@ end
 Base.eltype(::Type{NZChunks{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZChunks{It}}) where {It} = Base.EltypeUnknown()
 Base.IteratorSize(::Type{<:NZChunks}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZChunks}) = 1
 Base.length(it::NZChunks) = nnzchunks(it.itr)
 Base.size(it::NZChunks) = (nnzchunks(it.itr),)
 #Iterators.reverse(it::NZChunks) = NZChunks(Iterators.reverse(it.itr))
@@ -796,6 +799,7 @@ end
 Base.eltype(::Type{NZChunksPairs{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZChunksPairs{It}}) where {It} = Base.EltypeUnknown()
 Base.IteratorSize(::Type{<:NZChunksPairs}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZChunksPairs}) = 1
 Base.length(it::NZChunksPairs) = nnzchunks(it.itr)
 Base.size(it::NZChunksPairs) = (nnzchunks(it.itr),)
 #Iterators.reverse(it::NZChunksPairs) = NZChunksPairs(Iterators.reverse(it.itr))
@@ -817,6 +821,7 @@ end
 Base.eltype(::Type{NZIndices{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZIndices{It}}) where {It} = Base.EltypeUnknown()
 Base.IteratorSize(::Type{<:NZIndices}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZIndices}) = 1
 Base.length(it::NZIndices) = nnz(it.itr)
 Base.size(it::NZIndices) = (nnz(it.itr),)
 #Iterators.reverse(it::NZIndices) = NZIndices(Iterators.reverse(it.itr))
@@ -839,6 +844,7 @@ end
 Base.eltype(::Type{NZValues{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZValues{It}}) where {It} = Base.IteratorEltype(It)
 Base.IteratorSize(::Type{<:NZValues}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZValues}) = 1
 Base.length(it::NZValues) = nnz(it.itr)
 Base.size(it::NZValues) = (nnz(it.itr),)
 #Iterators.reverse(it::NZValues) = NZValues(Iterators.reverse(it.itr))
@@ -863,6 +869,7 @@ end
 Base.eltype(::Type{NZValuesView{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZValuesView{It}}) where {It} = Base.IteratorEltype(It)
 Base.IteratorSize(::Type{<:NZValuesView}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZValuesView}) = 1
 Base.length(it::NZValuesView) = nnz(it.itr)
 Base.size(it::NZValuesView) = (nnz(it.itr),)
 #Iterators.reverse(it::NZValuesView) = NZValuesView(Iterators.reverse(it.itr))
@@ -884,6 +891,7 @@ end
 Base.eltype(::Type{NZPairs{It}}) where {It} = eltype(It)
 Base.IteratorEltype(::Type{NZPairs{It}}) where {It} = Base.EltypeUnknown()
 Base.IteratorSize(::Type{<:NZPairs}) = Base.HasShape{1}()
+Base.ndims(::Type{<:NZPairs}) = 1
 Base.length(it::NZPairs) = nnz(it.itr)
 Base.size(it::NZPairs) = (nnz(it.itr),)
 #Iterators.reverse(it::NZPairs) = NZPairs(Iterators.reverse(it.itr))
